9d05f3f020c5eb281d8b44683287deb95b66c870
nicolaslps <ybozze150@gmail.com>
2020-10-09 09:57:39 +0200

    comment

--- Test/FizzBuzzTest.java
+++ Test/FizzBuzzTest.java
@@ -7,21 +7,25 @@ public class FizzBuzzTest {
 
     protected FizzBuzz test;
 
+    //Initialisation des choses qui seront répétées dans tous les tests
     @Before
     public void setup(  ) {
         test = new FizzBuzz();
     }
 
+    //Vérifie que la méthode isMultiple3 retourne true pour 3
     @Test
     public void AssertToBeMultipleOf3(){
         assertEquals(true, test.isMultiple3(3) );
     }
 
+    //Vérifie que la méthode isMultiple5 retourne true pour 3
     @Test
     public void AssertToBeMultipleOf5(){
         assertEquals(true, test.isMultiple5(5) );
     }
 
+    //Vérifie que la méthode isMultiple3And5 retourne true pour 15
     @Test
     public void AssertToBeMultipleOf3And5(){
         assertEquals(true, test.isMultiple3And5(15) );
--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,5 +1,6 @@
 public class FizzBuzz {
 
+    //vérifier si le nombre est un multiple de 3
     public static boolean isMultiple3(int nombre){
         if(nombre % 3 == 0){
             return true;
@@ -8,6 +9,7 @@ public class FizzBuzz {
         }
     }
 
+    //vérifier si le nombre est un multiple de 5
     public static boolean isMultiple5(int nombre){
         if(nombre % 5 == 0){
             return true;
@@ -16,6 +18,7 @@ public class FizzBuzz {
         }
     }
 
+    //vérifier si le nombre est un multiple de 3 et de 5
     public static boolean isMultiple3And5(int nombre){
         if(isMultiple3(nombre) && isMultiple5(nombre)){
             return true;
@@ -25,17 +28,21 @@ public class FizzBuzz {
     }
 
     public static void main(String[] args) {
+        //Boucle pour traiter les nombres de 1 à 100
         for (int nombre = 1 ; nombre <= 100 ; nombre ++){
-
+            //si le nombre est un multiple de 3 et 5
             if(isMultiple3And5(nombre)){
                 System.out.println(nombre + " FizzBuzz");
             }
+            //si le nombre est un multiple de 3
             else if(isMultiple3(nombre)){
                 System.out.println(nombre + " Fizz");
             }
+            //si le nombre est un multiple de 5
             else if(isMultiple5(nombre)){
                 System.out.println(nombre + " Buzz");
             }
+            //dans tous les cas
             else{
                 System.out.println(nombre);
             }
