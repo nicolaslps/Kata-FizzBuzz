fce05a2e7ab3ab7cefde582a05107c7b5d898eac
nicolaslps <ybozze150@gmail.com>
2020-10-09 10:01:54 +0200

    refactor test

--- Test/FizzBuzzTest.java
+++ Test/FizzBuzzTest.java
@@ -4,30 +4,21 @@ import static org.junit.Assert.*;
 
 
 public class FizzBuzzTest {
-
-    protected FizzBuzz test;
-
-    //Initialisation des choses qui seront répétées dans tous les tests
-    @Before
-    public void setup(  ) {
-        test = new FizzBuzz();
-    }
-
     //Vérifie que la méthode isMultiple3 retourne true pour 3
     @Test
     public void AssertToBeMultipleOf3(){
-        assertEquals(true, test.isMultiple3(3) );
+        assertTrue( FizzBuzz.isMultiple3(3) );
     }
 
     //Vérifie que la méthode isMultiple5 retourne true pour 3
     @Test
     public void AssertToBeMultipleOf5(){
-        assertEquals(true, test.isMultiple5(5) );
+        assertTrue( FizzBuzz.isMultiple5(5) );
     }
 
     //Vérifie que la méthode isMultiple3And5 retourne true pour 15
     @Test
     public void AssertToBeMultipleOf3And5(){
-        assertEquals(true, test.isMultiple3And5(15) );
+        assertTrue(FizzBuzz.isMultiple3And5(15) );
     }
 }
